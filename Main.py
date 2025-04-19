6955add872d3fde2ffe7f3116c8afed43a2705da
Blaze Sanders <blaze.d.a.sanders@gmail.com>
2025-04-19 06:10:17 -0700

    Add three level of imports and define Main function

--- Main.py
+++ Main.py
@@ -1,1 +1,23 @@
-sudo apt-get install sqlite3
+# Install SQLite system wide in Raspberry Pi 500 or Compute Module 4 (CM4) usint "sudo apt-get install sqlite3" command
+
+## Standard Python libraries
+import time                         # https://docs.python.org/3/library/time.html
+import logging                      # https://docs.python.org/3/library/time.html
+import subprocess                   # https://docs.python.org/3/library/subprocess.html
+from subprocess import Popen, PIPE  # TODO
+from subprocess import check_call   # TODO
+
+
+## 3rd party libraries
+# TODO
+
+## Internal libraries
+import * from EngineSoundGenerator
+import GlobalConstants as GC
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
